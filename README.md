125cba16839e715ab6d69cf8fe6b72c804ce6882
Mike Waldner <mike.waldner@redfall.com>
2015-09-22 21:52:28 -0600

    Initial commit. Sleep endpoint

---
+++ README.md
@@ -0,0 +1,7 @@
+# Waiter
+
+"A person who waits for a time, event, or opportunity."
+
+curl -X POST https://waiter.url/wait   -> wait 1 second
+curl -X POST https://waiter.url/wait?seconds=5   -> wait 5 second
+
---
+++ cmd/waiter/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.POST("/wait", func(c *gin.Context) {
+		second_str := c.DefaultQuery("seconds", "1")
+		seconds, _ := strconv.Atoi(second_str)
+		time.Sleep(time.Duration(seconds) * time.Second)
+		c.String(http.StatusOK, "Done")
+	})
+
+	router.Run(":" + port)
+}
